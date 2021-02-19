8f3b61416ea325dda7f5d7cb178a98ec18a3ebec
meaning2415 <meaning2415@gmail.com>
2021-02-19 17:10:14 +0900

    chapter 7

--- index.js
+++ index.js
@@ -2,9 +2,32 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require("body-parser")
+const { User } = require("./models/User")
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}))
+
+//application/json
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Jimin:qwer1234@meaning.bm8ak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlparser:true, useUnifiedTopology:true,useCreateIndex:true, useFindAndModify:false}).then(()=>console.log('MongoDB Connected...')).catch(err => console.log(err))
+mongoose.connect('mongodb+srv://Jimin:qwer1234@meaning.bm8ak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser:true, useUnifiedTopology:true,useCreateIndex:true, useFindAndModify:false}).then(()=>console.log('MongoDB Connected...')).catch(err => console.log(err))
+
+
 
 app.get('/',(req,res) => res.send('Hello Wordl!'))
+
+app.post('/register',(req,res)=>{
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({success:false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+
+})
+
 
 app.listen(port,()=>console.log(`Example app listening on port ${port}!`))
